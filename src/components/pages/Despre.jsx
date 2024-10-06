d076fe93f6cf6bd088075ffef75c2a5e388a338f
MartianKiss <161372292+MartianKiss@users.noreply.github.com>
2024-10-06 20:07:48 +0300

    progres cu pagina Despre

--- src/components/pages/Despre.jsx
+++ src/components/pages/Despre.jsx
@@ -1,10 +1,43 @@
-
+import "../../styling/Despre.css"
 
 const Despre = () =>{
     return(
         <div>
-            <h1>Pagina Despre</h1>
-            <p>Aceasta este pagina despre noi.</p>
+            <div className="container0">
+            
+                <div className="descriere">
+                    <h1>Povestea noastra</h1>
+                    <p>La noi la cabana este un proiect de suflet care s-a născut din dorința de
+                         a restaura o casa veche din Munții Apuseni, care inițial a fost gândita ca 
+                         o casa de vacanta a familiei si care ulterior a fost pusa in circuitul 
+                         turistic pentru a împărtăși si altora frumusețea si energia acestui loc
+                         </p>
+
+                         <p>Suntem doi artisti plastici iubitori de vechi si traditie, care am luat
+                          aceasta lucrare ca o provocare, investind timp, munca si atentie la 
+                          detalii. Am pastrat o multime de elemente originale insa am adus si 
+                          elemente actuale care ofera casutei confort.</p>
+                          
+                          <p>Locatia este intr-o zona incarcata de istorie, mai exact in comuna Horea,
+                          un loc foarte pitoresc unde te poti bucura de o priveliste care iti
+                         taie respiratia direct din dormitor. </p>
+                </div>
+                <div className="container1">
+                    <div className="container_img">poveste</div>
+                    <div className="descriere-suplim">
+                        <div className="descriere2">
+                            <h2></h2>
+                            <p>Aceasta este pagina despre noi.</p>
+                        </div>
+                        <div className="descriere3">
+                            <h2></h2>
+                            <p>Aceasta este pagina despre noi.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            
         </div>
         
         
